4264520c0fd10d5a8a510f8aa039dcdd31c3e55b
sanjomaru <sanjomaruwork13@gmail.com>
2025-01-10 23:50:31 +0800

    - Conscructor, Destructor, Magic Classes practice.

--- index.php
+++ index.php
@@ -2,13 +2,39 @@
 
 class User{
     public $name;
+    public $age;
 
-    public function statement(){
-        return $this->name.' Says Hi';
+    //This will run if object is instantiated
+    public function __construct($name = '', $age = ''){
+
+        // __Class__ is a magic constant
+        echo __CLASS__.' has been instantiated.<br>';
+
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function statement($name){
+        return $this->$name.' Says Hi';
+    }
+    
+    //This will run after the object is used, or object is not referenced
+    // Used for cleanup, closing connections etc
+    public function __destruct(){
+        echo "<br>";
+        echo "Destructor Ran..";
     }
 }
 
-$user = new User();
-echo $user->name = 'Sanjo';
+// $user = new User();
+// echo $user->name = 'Sanjo';
+// echo "<br>";
+// echo $user->statement();
+
+//FOR CONSTRUCTION PRACTICE
+$user1 = new User('Sanjo', 10);
+echo $user1->name.' is '.$user1->age.' yrs old.';
 echo "<br>";
-echo $user->statement();
+
+$user2 = new User('Mary');
+echo $user2->name;
