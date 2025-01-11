a7cceeeff13b21085e985b5c61ee3b29ff0f7cc9
sanjomaru <sanjomaruwork13@gmail.com>
2025-01-11 23:47:00 +0800

    - Added Getters and Setters

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP MVC</title>
+</head>
+<body>
+    
 <?php
 
 class User{
@@ -38,3 +48,76 @@ echo "<br>";
 
 $user2 = new User('Mary');
 echo $user2->name;
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+?>
+
+<h1>Getters and Setters</h1>
+
+<?php
+//GETTERS AND SETTERS 
+class newUser{
+    private $name;
+    private $age;
+
+    public function __construct($name = '', $age = ''){
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    //For the getter just create a new method to access the private property
+    public function getName(){
+        return $this->name;
+    }
+
+    public function getAge(){
+        return $this->age;
+    }
+
+    //Same consept with setter, just add a property requirement on the method
+    public function setName($name){
+        return $this->name = $name;
+    }
+
+    public function setAge($age){
+        return $this->age = $age;
+    }
+
+    //There is a magic method called __get , use this so that you dont need to create multiple method for each property
+    public function __get($property){
+        if(property_exists($this, $property)){
+            return $this->$property;
+        }
+    }
+    
+    //This is the same concept with __set
+    public function __set($property, $value){
+        if(property_exists($this, $property)){
+            $this->$property = $value;
+        }
+        return $this;
+    }
+}
+
+$newUser = new newUser('Sanjo', 100);
+$newUser->setName('kirito');
+$newUser->setAge(1200);
+echo $newUser->getAge();
+echo "<br>";
+echo $newUser->getName();
+echo "<br>";
+?>
+<h3>This is a magic property __get()</h3>
+<?php
+
+$newUser->__set('name','Zorro');
+$newUser->__set('age','1123');
+echo $newUser->__get('name');
+echo "<br>";
+echo $newUser->__get('age');
+?>
+
+</body>
+</html>
